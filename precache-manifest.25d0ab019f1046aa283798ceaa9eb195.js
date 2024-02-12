b11a203f96f15bc0866826677d639ac30ac987ba
John Doe <k6473870@gmail.com>
2024-02-12 14:04:21 +0530

    Updates

---
+++ precache-manifest.25d0ab019f1046aa283798ceaa9eb195.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "521613af3519288a696a61914a1317e7",
+    "url": "/Myportfolio/index.html"
+  },
+  {
+    "revision": "6d23247a6be85544b1db",
+    "url": "/Myportfolio/static/css/main.ec6104f3.chunk.css"
+  },
+  {
+    "revision": "ff885841d30740be31dd",
+    "url": "/Myportfolio/static/js/2.833589c5.chunk.js"
+  },
+  {
+    "revision": "8738f57d8640c3e731e9dff9903d3fa9",
+    "url": "/Myportfolio/static/js/2.833589c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d23247a6be85544b1db",
+    "url": "/Myportfolio/static/js/main.2be2629d.chunk.js"
+  },
+  {
+    "revision": "029949ddad56970791f1",
+    "url": "/Myportfolio/static/js/runtime-main.6239034d.js"
+  },
+  {
+    "revision": "b28cb3665df82e95830541c3e16ddb39",
+    "url": "/Myportfolio/static/media/exp.b28cb366.jpeg"
+  },
+  {
+    "revision": "ab815d2d02b634db8ecc464e77a74241",
+    "url": "/Myportfolio/static/media/p1,1.ab815d2d.PNG"
+  },
+  {
+    "revision": "52879d9ad6106c0852f33cacec37e3d7",
+    "url": "/Myportfolio/static/media/p1,2.52879d9a.PNG"
+  },
+  {
+    "revision": "5ff80fafed2c2423f687be50151d17ca",
+    "url": "/Myportfolio/static/media/p1.5ff80faf.PNG"
+  },
+  {
+    "revision": "72ef6230ae9406492deb196e7b6e0c51",
+    "url": "/Myportfolio/static/media/p10 (1).72ef6230.jpeg"
+  },
+  {
+    "revision": "8cd6dba1035b2eb65fd259c5857886a0",
+    "url": "/Myportfolio/static/media/p11 (2).8cd6dba1.jpeg"
+  },
+  {
+    "revision": "159fa5c22af948222e47039f5e522eaa",
+    "url": "/Myportfolio/static/media/p2.159fa5c2.PNG"
+  },
+  {
+    "revision": "17eebf4fea11ee70820ae032ca0a503f",
+    "url": "/Myportfolio/static/media/p3.17eebf4f.PNG"
+  },
+  {
+    "revision": "e29dd19fe547961b6e60067ccec54d43",
+    "url": "/Myportfolio/static/media/p4.e29dd19f.PNG"
+  },
+  {
+    "revision": "46de0af4eade85bcc36228738c011b98",
+    "url": "/Myportfolio/static/media/p5.46de0af4.PNG"
+  },
+  {
+    "revision": "30c8bba7260d77fd9cd76169123a13e2",
+    "url": "/Myportfolio/static/media/p7.30c8bba7.PNG"
+  },
+  {
+    "revision": "c8a1fb7e888f70b8b20eead976f0f46f",
+    "url": "/Myportfolio/static/media/p8.c8a1fb7e.PNG"
+  },
+  {
+    "revision": "aacca118400efcdca2cba26207d98a06",
+    "url": "/Myportfolio/static/media/p9.aacca118.PNG"
+  },
+  {
+    "revision": "496620dd3cb60e5fc6643e363166da0f",
+    "url": "/Myportfolio/static/media/skill.496620dd.jpg"
+  }
+]);
